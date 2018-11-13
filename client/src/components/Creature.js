acdcef158f07391a486d83f55cda12d87260ddf7
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-11-13 09:35:59 -0500

    delete function

--- client/src/components/Creature.js
+++ client/src/components/Creature.js
@@ -4,13 +4,23 @@ import styled from 'styled-components'
 
 class Creature extends Component {
   state = {
-
+    creature: {}
   }
   componentDidMount(){
     // Make an api call to get one single user
-    const userId = this.props.match.params.userId
-    axios.get(`/api/users/${userId}`).then(res => {
-      this.setState({user: res.data})
+    const creatureId = this.state.match.params.creatureId
+    axios.get(`/api/creatures/${creatureId}`).then(res => {
+      this.setState({creature: res.data})
+    })
+  }
+
+  deleteThisCreature = (creatureId) => {
+    axios.delete(`/api/creatures/${creatureId}`).then(() => {
+      const newCreatures = [...this.state.creatures]
+      const filtered = newCreatures.filter(creature => {
+        return creature._id !== creatureId
+      })
+      this.setState({creatures: filtered})
     })
   }
 
@@ -19,8 +29,8 @@ class Creature extends Component {
       <div className="creature-info-div">
         <h4>{this.props.name}</h4>
         <h5>Decription: {this.props.description}</h5>
-        <img src={this.props.img}></img>
-        <button>Delete</button>
+        <img src={this.props.img} alt=""></img>
+        <button onClick={this.deleteThisCreature}>Delete</button>
       </div>    
     )
   }
