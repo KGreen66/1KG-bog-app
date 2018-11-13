24f050e55d9d8be1cb5307069478b204bd41d0db
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-11-13 08:58:35 -0500

    single

--- client/src/components/Creature.js
+++ client/src/components/Creature.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import styled from 'styled-components'
 
 class Creature extends Component {
+  state = {
+
+  }
+  componentDidMount(){
+    // Make an api call to get one single user
+    const userId = this.props.match.params.userId
+    axios.get(`/api/users/${userId}`).then(res => {
+      this.setState({user: res.data})
+    })
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <div className="creature-info-div">
+        <h4>{this.props.name}</h4>
+        <h5>Decription: {this.props.description}</h5>
+        <img src={this.props.img}></img>
+        <button>Delete</button>
+      </div>    
     )
   }
 }
